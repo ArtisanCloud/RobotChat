737d93c704b25cf8f454f6e4ed58760f0560488b
Matrix-X <hbj418@gmail.com>
2023-07-13 14:09:49 +0800

    refact(artbot): get lora fields

--- robots/kernel/model/lora.go
+++ robots/kernel/model/lora.go
@@ -1,12 +1,17 @@
 package model
 
-import "github.com/ArtisanCloud/PowerLibs/v3/database"
+import (
+	"github.com/ArtisanCloud/PowerLibs/v3/database"
+	"gorm.io/datatypes"
+)
 
 type Lora struct {
 	database.PowerModel
 
-	Preview string `gorm:"comment:预览图" json:"preview"`
-	Hash    string `gorm:"comment:Hash" json:"hash"`
-	Alias   string `gorm:"comment:别名" json:"alias"`
-	Name    string `gorm:"comment:名称" json:"name"`
+	Preview  string         `gorm:"comment:预览图" json:"preview"`
+	Hash     string         `gorm:"comment:Hash" json:"hash"`
+	Alias    string         `gorm:"comment:别名" json:"alias"`
+	Name     string         `gorm:"comment:名称" json:"name"`
+	Path     string         `gorm:"comment:路径" json:"path"`
+	MetaData datatypes.JSON `gorm:"comment:元数据" json:"metadata"`
 }
